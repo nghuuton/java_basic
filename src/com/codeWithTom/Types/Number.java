e8294967b061f352e72c20d0fcaa69a193d06d43
Tôn <nghuuton@gmail.com>
2021-03-19 18:29:32 +0700

    Type Numbers

--- src/com/codeWithTom/Types/Number.java
+++ src/com/codeWithTom/Types/Number.java
@@ -2,11 +2,15 @@ package com.codeWithTom.Types;
 
 public class Number {
     public static void main(String[] args) {
+
+        // Integer
         byte number = 1; // (1byte)
         short numberS = 3; // (2byte)
         int age = 23; // (4byte)
-        float price = 133F; // (4byte)
         long numberL = 133L; // (8byte)
+        char a = 2; // (4byte) Unicode
+        // Floating
+        float price = 133F; // (4byte)
         double percent = 100D; //(8byte)
 
         System.out.println(number);
