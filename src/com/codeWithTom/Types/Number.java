5b282b8cecf1be4d2f15c9a74254d03085b0bcf2
Tôn <nghuuton@gmail.com>
2021-03-19 18:26:23 +0700

    Types Number

---
+++ src/com/codeWithTom/Types/Number.java
@@ -0,0 +1,20 @@
+package com.codeWithTom.Types;
+
+public class Number {
+    public static void main(String[] args) {
+        byte number = 1; // (1byte)
+        short numberS = 3; // (2byte)
+        int age = 23; // (4byte)
+        float price = 133F; // (4byte)
+        long numberL = 133L; // (8byte)
+        double percent = 100D; //(8byte)
+
+        System.out.println(number);
+        System.out.println(numberS);
+        System.out.println(age);
+        System.out.println(price);
+        System.out.println(percent);
+        System.out.println(numberL);
+
+    }
+}
